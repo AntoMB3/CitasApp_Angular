058b1fd6e36c382c024d197405a5d87f5e076577
Anto <anto.munbar3@gmail.com>
2024-03-19 07:22:21 -0600

    [FIX] Update current user when upload new image

--- SPA/src/app/register/register.component.ts
+++ SPA/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,12 +11,12 @@ import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Vali
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
   registerForm: FormGroup = new FormGroup({});
   maxDate: Date = new Date();
+  validationErrors: string[] | undefined;
   
   constructor(private accountService: AccountService,
-    private toastr: ToastrService, private fb: FormBuilder) {}
+    private toastr: ToastrService, private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
     this.initializeForm();
@@ -47,20 +48,30 @@ export class RegisterComponent implements OnInit {
   }
 
   register(): void {
-    console.log(this.registerForm?.value)
-    // this.accountService.register(this.model).subscribe({
-    //   next: () => {
-    //     this.cancel();
-    //   },
-    //   error: error => {
-    //     this.toastr.error(error.error);
-    //     console.log(error);
-    //   }
-    // });
+    const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
+    const values = {...this.registerForm.value, dateOfBirth: dob}  
+    console.log(dob)  
+    console.log(values)  
+    this.accountService.register(values).subscribe({
+      next: () => {
+        this.router.navigateByUrl("/members");
+      },
+      error: error => {
+        console.log(error);
+        this.validationErrors = error;
+      }
+    });
   }
 
   cancel(): void {
     this.cancelRegister.emit(false);
   }
 
+  private getDateOnly(dob: string | undefined){
+    if (!dob) return;
+
+    let newdob = new Date(dob);
+    return new Date(newdob.setMinutes(newdob.getMinutes() - newdob.getTimezoneOffset()))
+      .toISOString().substring(0,10);
+  }
 }
