76ce740ca7c99f7b8e0b640d5d900f5f248688c1
Anto <anto.munbar3@gmail.com>
2024-05-22 07:06:35 -0600

    Tercer Parcial - Member messages detail component

--- SPA/src/app/members/member-messages/member-messages.component.ts
+++ SPA/src/app/members/member-messages/member-messages.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Message } from 'src/app/_models/messages';
+import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-messages',
+  standalone: true,
   templateUrl: './member-messages.component.html',
-  styleUrls: ['./member-messages.component.css']
+  styleUrls: ['./member-messages.component.css'],
+  imports: [CommonModule]
 })
-export class MemberMessagesComponent {
+export class MemberMessagesComponent implements OnInit {
+  @Input() username?: string;
+  messages: Message[] = [];
+
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    if (this.username) {
+      this.messageService.getMessageThread(this.username).subscribe({
+        next: messages => this.messages = messages
+      })
+    }
+  }
 
 }
